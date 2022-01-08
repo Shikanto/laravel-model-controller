282243056e0488e41f70b1ecf28d93d292b6d5ba
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2022-01-08 12:58:51 +0100

    structure ex

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class= "text-center">TOP 10 FILM BOOLEAN</h1>
+        <div class="container d-flex flex-wrap gap-3 justify-content-center">
+        @foreach ($movie as $info_movie)
+        <div class="card text-center" style="width: 18rem;">
+            <div class="card-body">
+                <h6>titolo</h6>
+                <h1 class="card-title">{{$info_movie['title']}}</h1>
+                <h6>titolo originale</h6>
+                <h2 class="card-title">{{$info_movie['original_title']}}</h2>
+                <p class="card-text"><span class="small-title">Film Nationality:</span> {{$info_movie['nationality']}} <br>
+                <span class="small-title">Data di rilascio:</span> {{$info_movie['date']}} <br>
+                <span class="small-title">Voto FIlm:</span> {{$info_movie['vote']}}
+                </p>
+                <a href="https://it.wikipedia.org/wiki/{{$info_movie['title']}}" class="btn btn-info">Check the full info Film</a>
+            </div>
+        </div>       
+        @endforeach
+        </div>
+    </div>
+</body>
+</html>
